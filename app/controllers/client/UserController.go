95ba9c8f8a74ca09d24a6d233c90f202bc494a05
amir sobhani <devil.infamous@gmail.com>
2023-06-25 16:39:42 +0330

    check count of otp request

--- app/controllers/client/UserController.go
+++ app/controllers/client/UserController.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/datatypes"
 	"math/rand"
+	"time"
 )
 
 func Signup(c *fiber.Ctx) error {
@@ -27,12 +28,26 @@ func Signup(c *fiber.Ctx) error {
 		})
 	}
 
+	timeQuery := time.Now().Add(time.Minute * -4)
+	var countOtp int64
+	database.DB.Model(&models.Otp{
+		Mobile: user.Mobile,
+	}).Where("created_at >= ?", timeQuery).Count(&countOtp)
+
+	if countOtp > 4 {
+		return infastructure.Output(c, &infastructure.OutputStruct{
+			Message: "too many request please later",
+			Status:  fiber.StatusTooManyRequests,
+		})
+	}
+
 	min := 1000
 	max := 8999
 	token := rand.Intn(rand.Intn(max)) + min
 
 	otp := models.Otp{
 		Token:    token,
+		Mobile:   user.Mobile,
 		TempData: datatypes.NewJSONType(*user),
 	}
 
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"github.com/amirsobhani/melk_back/database"
 	"github.com/amirsobhani/melk_back/route"
 	"github.com/gofiber/fiber/v2"
@@ -27,21 +26,13 @@ func main() {
 		// Override default error handler
 		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
 			// Status code defaults to 500
-			code := fiber.StatusInternalServerError
 
 			// Retrieve the custom status code if it's a *fiber.Error
 			var e *fiber.Error
 			if errors.As(err, &e) {
-				code = e.Code
-			}
-
-			// Send custom error page
-			err = ctx.Status(code).SendFile(fmt.Sprintf("./%d.html", code))
-			if err != nil {
-				// In case the SendFile fails
 				return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 					"data":    nil,
-					"message": "Internal Server Error",
+					"message": err.Error(),
 				})
 			}
 
